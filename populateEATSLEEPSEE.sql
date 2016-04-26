66797bd91b2914e2d355ca039f786b6e66f6180b
sabniss <sabniss@bc.edu>
2016-04-25 22:08:04 -0400

    Create populateEATSLEEPSEE.sql

---
+++ populateEATSLEEPSEE.sql
@@ -0,0 +1,35 @@
+Populating the SIGNUP table:
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (1, 'Shannon', 'Specht', 'spechts@bc.edu', 'dEmonm1sty,' 'Boston College', 'Computer Science');
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (2, 'Erin', 'Comber', 'combere@bc.edu', 'M1K3TYS0N', 'Boston College', 'Elementary Education');
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (3, 'Abigail', 'Kopec', 'kopeca@bc.edu', 'y0s3mitef1tzg3rald', 'Boston College', 'Physics');
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (4, 'Andrew', 'Gaffney', 'gaffneya@bc.edu', 'b3th0v3nR0X', 'Boston College', 'Music');
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (5, 'Garrett', 'Lau', 'laug@bc.edu', 'l1ttl3b0', 'Boston College', 'International Studies');
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (6, 'Anthony', 'Marino', 'marinoa@bc.edu', 'cr3wluvR', 'Boston College', 'Mathematics');
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (7, 'Devina', 'Sabnis', 'sabnisdev@syr.edu', 'n1ckym0use', 'Syracuse University', 'Musical Theatre');
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (8, 'Prianca', 'Tawde', 'tawdep@mit.edu', 'fav0r1tech1ld', 'Massachusetts Institute of Technology', 'Chemistry');
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (9, 'Rhea', 'Mody', 'modyrhea@emory.edu', 'z1ggyz00', 'Emory University', 'Business');
+insert into SIGNUP (ID, FirstName, LastName, Email, Password, College, Major) values (10, 'Emily', 'Stout', 'stoute@bc.edu', 'MaGnUmx3', 'Duke University', 'French');
+
+Populating the ENTRIES table:
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (1, 'Vienna', 'Eat', 'Vinothek W-Einkehr', 5, 'Great place to taste local wines');
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (2, 'Quito', 'See', 'La Compañía', 4, 'The church is breathtaking');
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (3, 'Prague', 'Sleep', 'Pachtuv', 4, 'Always a pleasure to stay at');
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (4, 'Rome', 'Eat', 'Mignanelli 24', 1, 'The worst restaurant in Rome');
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (5, 'London', 'Eat', 'Sketch', 5, 'Great place!');
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (6, 'Copenhagen', 'See', 'Alternative Christiania', 2, 'The Amish of Denmark. Very boring.');
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (7, 'Bombay', 'Sleep', 'The Taj Mahal Palace', 5, 'Enchanting and historical');
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (8, 'Brussels', 'See', 'Musical Instruments Museum', 3, 'Electronic tour guide did not work');
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (9, 'Berlin', 'Sleep', 'Hotel Adlon Kempinski', 4, 'A trip back in time with modern additions');
+insert into ENTRIES (ID, City, EatSleepOrSee, EstablishmentName, Stars, Commments) values (10, 'Paris', 'See', 'Eiffel Tower', 2, 'Overrated and disappointing');
+
+Populating the CONTACTUS table:
+insert into CONTACTUS (ID, FirstName, LastName, Email, Comments) values (11, ‘Addie’, ‘Diradoorian’, ‘diradoor@bc.edu’, ‘Love this site!’);
+insert into CONTACTUS (ID, FirstName, LastName, Email, Comments) values (12, ‘Shukra’, ‘Sabnis’, ‘sabniss@bc.edu’, ‘This site is so easy to use!’);
+insert into CONTACTUS (ID, FirstName, LastName, Email, Comments) values (13, ‘Christine’, ‘Kenyon’, ‘kenyonc@bc.edu’, ‘This site is amazing!’);
+
+
+
+
+
+
+
