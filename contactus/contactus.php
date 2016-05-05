5540e4c69fa9de916a54d157e249a2b77584f5a2
Christine Kenyon <kenyonca@bc.edu>
2016-05-05 02:16:14 -0400

    Update contactus.php

--- contactus/contactus.php
+++ contactus/contactus.php
@@ -1,24 +1,117 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8" />
-	<title>Contact Us</title>
-	<h1>Contact Us</h1>
-	<style type = "text/css">		  
-			body {font-family: Avenir;}
-	</style>
+<title>Contact Us</title>
+<meta charset="utf-8">
+<link rel="icon" href="images/favicon.ico">
+<link rel="shortcut icon" href="images/favicon.ico">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.1.1.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.equalheights.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/jquery.ui.totop.js"></script>
+<script>
+$(window).load(function () {
+    $().UItoTop({
+        easingType: 'easeOutQuart'
+    });
+});
+</script>
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<link rel="stylesheet" media="screen" href="css/ie.css">
+<![endif]-->
+<div class="out"><a href="logout.php">Log Out</a></div>
+<div class="sign"><a href="newentry.php">Submit a Review!</div></a>
 </head>
+
+
 <body>
+<header>
+  <div class="container_12">
+    <div class="grid_12">
+      <h1><a href="home.html"><img src="images/logo.png" alt="Eat Sleep See"></a></h1>
+      <div class="clear"></div>
+    </div>
+    <div class="menu_block">
+      <nav>
+        <ul class="sf-menu">
+          <li class="current"><a href="home.html">Home</a></li>
+           <li class="with_ul"><a>London</a>
+            <ul>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/londoneat.php">Eat</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/londonsleep.php">Sleep</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/londonsee.php"">See</a></li>
+            </ul>
+          </li>
+          <li class="with_ul"><a>Paris</a>
+            <ul>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/pariseat.php">Eat</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/parissleep.php">Sleep</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/parissee.php">See</a></li>
+            </ul>
+          </li>          
+	  <li class="with_ul"><a>Rome</a>
+            <ul>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/romeeat.php">Eat</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/romesleep.php">Sleep</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/romesee.php">See</a></li>
+            </ul>
+          </li>
+          <li class="with_ul"><a>Sydney</a>
+            <ul>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/sydneyeat.php">Eat</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/sydneysleep.php">Sleep</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/sydneysee.php">See</a></li>
+            </ul>
+          </li>
+          <li class="with_ul"><a>Tokyo</a>
+            <ul>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/tokyoeat.php">Eat</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/tokyosleep.php">Sleep</a></li>
+              <li><a href="http://cscilab.bc.edu/~diradoor/final/tokyosee.php">See</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <div class="clear"></div>
+    </div>
+    <div class="clear"></div>
+  </div>
+
+</header>
+
+
+
+<div class="main">
+  <div class="content">
+ <div class="container_12">
+
 <?php
 if (isset($_GET['submit']) && isset($_GET['name']) && isset($_GET['email'])
-&& isset($_GET['eomments'])) {
+&& isset($_GET['comments'])) {
 handle_contact_form();
 } else {
 	echo "Fill out all of the fields!";}
 display_contact_form();
 
 ?>
+
+
+
+
+      <div class="grid_9">
+
+
+</div>
+</div>
+</div>
+</div>
 </body>
+</html>
+
 
 <?php
 function display_contact_form(){
@@ -76,4 +169,4 @@ function handle_contact_form(){
 	}
 	//disconnect_from_db( $dbc, $result );
 }
-
+?>
