7d42310688c2944a7701a2105711e49666948974
addiediradoorian <addiediradoorian@gmail.com>
2016-05-05 01:07:40 -0400

    Create logout.php

---
+++ logout/logout.php
@@ -0,0 +1,19 @@
+<?php
+setcookie('MyCookie', 0, time()-3600);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Log out</title>
+	<style type = "text/css">		  
+			body {font-family: Verdana, sans-serif;}
+	</style>
+</head>
+<body>
+</body>
+
+<a href="access.html">Logout</a>
+
+</body>
+</html>
