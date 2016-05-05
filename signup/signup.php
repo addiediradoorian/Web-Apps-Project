0bc8e0c35f9d862ce6b3a3f6d7348bebd2a16077
Christine Kenyon <kenyonca@bc.edu>
2016-05-05 02:19:07 -0400

    Update signup.php

--- signup/signup.php
+++ signup/signup.php
@@ -1,16 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Eat Sleep See | Sign Up</title>
+<meta charset="utf-8">
+<link rel="icon" href="images/favicon.ico">
+<link rel="shortcut icon" href="images/favicon.ico">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.1.1.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.equalheights.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/jquery.ui.totop.js"></script>
+<script>
+$(window).load(function () {
+    $().UItoTop({
+        easingType: 'easeOutQuart'
+    });
+});
+</script>
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<link rel="stylesheet" media="screen" href="css/ie.css">
+<![endif]-->
+<div class="sign"><a href="http://cscilab.bc.edu/~diradoor/final/newentry/newentry.php">Submit a Review!</a></div>
+</head>
+
+
+<body>
+<header>
+  <div class="container_12">
+    <div class="grid_12">
+      <h1><a href="home.html"><img src="images/logo.png" alt="Eat Sleep See"></a></h1>
+      <div class="clear"></div>
+    </div>
+    <div class="menu_block">
+      <nav>
+        <ul class="sf-menu">
+          <li class="current"><a href="home.html">Home</a></li>
+          <li class="with_ul"><a>London</a>
+            <ul>
+              <li><a href="http://London/londoneat.php">Eat</a></li>
+              <li><a href="London/londonsleep.php">Sleep</a></li>
+              <li><a href="London/londonsee.php">See</a></li>
+            </ul>
+          </li>
+          <li class="with_ul"><a>Paris</a>
+            <ul>
+              <li><a href="Paris/pariseat.php">Eat</a></li>
+              <li><a href="Paris/parissleep.php">Sleep</a></li>
+              <li><a href="Paris/parissee.php">See</a></li>
+            </ul>
+          </li>          
+	  <li class="with_ul"><a>Rome</a>
+            <ul>
+              <li><a href="Rome/romeeat.php">Eat</a></li>
+              <li><a href="Rome/romesleep.php">Sleep</a></li>
+              <li><a href="Rome/romesee.php">See</a></li>
+            </ul>
+          </li>
+          <li class="with_ul"><a>Sydney</a>
+            <ul>
+              <li><a href="Sydney/sydneyeat.php">Eat</a></li>
+              <li><a href="Sydney/sydneysleep.php">Sleep</a></li>
+              <li><a href="Sydney/sydneysee.php">See</a></li>
+            </ul>
+          </li>
+          <li class="with_ul"><a>Tokyo</a>
+            <ul>
+              <li><a href="Tokyo/tokyoeat.php">Eat</a></li>
+              <li><a href="Tokyo/tokyosleep.php">Sleep</a></li>
+              <li><a href="Tokyo/tokyosee.php">See</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <div class="clear"></div>
+    </div>
+    <div class="clear"></div>
+  </div>
+
+</header>
+
+
+
+<div class="main">
+  <div class="content">
+ <div class="container_12">
+
 <?php
 function connect_to_db( $diradoor ){
 	$dbc = @mysqli_connect( "localhost", "diradoor", "nXze83Ks", $diradoor ) or
 			die( "Connect failed: ". mysqli_connect_error() );
 	return $dbc;
 }
-
 function disconnect_from_db( $dbc, $result ){
 	mysqli_free_result( $result ); //mysqli_free_result( $result );
 	mysqli_close( $dbc );
 }
-
-
 function perform_query( $dbc, $query ){
 	//echo $query;
 	$result = mysqli_query($dbc, $query) or 
@@ -24,15 +111,14 @@ function perform_query( $dbc, $query ){
 <head>
 	<meta charset="UTF-8" />
 	<title>Sign Up</title>
-	<h1>Eat Sleep See</h1>
 	<style type = "text/css">		  
 			body {font-family: Avenir;}
 	</style>
 </head>
 <body>
 <?php
-if (isset($_POST['submit']) && isset($_POST['firstname']) && isset($_POST['lastname'])
-&& isset($_POST['email']) && isset($_POST['password1']) && isset($_POST['password2'])) {
+if (isset($_GET['submit']) && isset($_GET['firstname']) && isset($_GET['lastname'])
+&& isset($_GET['email']) && isset($_GET['password1']) && isset($_GET['password2'])) {
 handle_form();
 } else {
 	echo "Fill out all of the fields!";}
@@ -62,11 +148,11 @@ function display_form(){
 
 function handle_form(){
 	$dbc = connect_to_db( "diradoor" );	
-	$firstnamevalue = isset($_POST['firstname']) ? $_POST['firstname'] : "";
-	$lastnamevalue = isset($_POST['lastname']) ? $_POST['lastname'] : "";
-	$emailvalue = isset($_POST['email']) ? $_POST['email'] : "";
-	$passwordvalue1 = isset($_POST['password1']) ? $_POST['password1'] : "";
-	$passwordvalue2 = isset($_POST['password2']) ? $_POST['password2'] : "";
+	$firstnamevalue = isset($_GET['firstname']) ? $_GET['firstname'] : "";
+	$lastnamevalue = isset($_GET['lastname']) ? $_GET['lastname'] : "";
+	$emailvalue = isset($_GET['email']) ? $_GET['email'] : "";
+	$passwordvalue1 = isset($_GET['password1']) ? $_GET['password1'] : "";
+	$passwordvalue2 = isset($_GET['password2']) ? $_GET['password2'] : "";
 	$encoded1 = sha1( $passwordvalue1 );
 	$encoded2 = sha1( $passwordvalue2 );
 	$query = "insert into signup (firstname, lastname, email, password1, password2) 
@@ -83,3 +169,24 @@ function handle_form(){
 	}
 	//disconnect_from_db( $dbc, $result );
 }
+?>
+</div>
+
+
+
+      <div class="grid_9">
+
+
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
